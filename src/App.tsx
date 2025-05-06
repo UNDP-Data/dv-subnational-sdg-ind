40537bf20a0d41b88fcd4031c468adee0e198a67
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2025-05-06 17:47:10 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { H3 } from '@undp/design-system-react';
+
+import '@/styles/fonts.css';
+import '@undp/design-system-react/dist/style.css';
+import '@undp/data-viz/dist/style.css';
+import undpLogo from './assets/undp-logo-blue.svg';
+
+function App() {
+  return (
+    <div className='m-5'>
+      <div>
+        <img
+          src={undpLogo}
+          className='logo react mb-8'
+          alt='React logo'
+          width='72px'
+          style={{ marginLeft: 'auto', marginRight: 'auto' }}
+        />
+      </div>
+      <H3 className='text-center'>
+        This is template for building visualization and frontend project
+        maintained by UNDP&apos;s SDG Integration Team.
+        <br />
+        <br />
+        Contact us at data@undp.org if you have any feedback or questions.
+      </H3>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint()],
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  server: {
+    cors: {
+      origin: '*',
+      methods: ['GET'],
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+});
